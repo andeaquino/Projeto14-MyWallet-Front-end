ee87da59f697ee1ee8ae956071c5d47a74addfb5
Anderson Aquino <andersonmr.aquino@gmail.com>
2021-11-28 16:23:23 -0300

    created context for user info

--- src/App.js
+++ src/App.js
@@ -1,28 +1,36 @@
 import GlobalStyle from "./shared/GlobalStyle";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { useState } from "react";
+import { UserContext } from "./contexts/UserContext";
 import Login from "./pages/Login/Login";
 import Register from "./pages/Register/Register";
 import Account from "./pages/Account/Account";
 import AddEntry from "./pages/AddEntry/AddEntry";
 
 function App() {
+  const [userInfo, setUserInfo] = useState(
+    JSON.parse(localStorage.getItem("user"))
+  );
+
   return (
     <BrowserRouter>
-      <GlobalStyle />
-      <Switch>
-        <Route exact path="/">
-          <Login />
-        </Route>
-        <Route exact path="/cadastro">
-          <Register />
-        </Route>
-        <Route exact path="/conta">
-          <Account />
-        </Route>
-        <Route exact path="/:entryType">
-          <AddEntry />
-        </Route>
-      </Switch>
+      <UserContext.Provider value={{ userInfo, setUserInfo }}>
+        <GlobalStyle />
+        <Switch>
+          <Route exact path="/">
+            <Login />
+          </Route>
+          <Route exact path="/cadastro">
+            <Register />
+          </Route>
+          <Route exact path="/conta">
+            <Account />
+          </Route>
+          <Route exact path="/:entryType">
+            <AddEntry />
+          </Route>
+        </Switch>
+      </UserContext.Provider>
     </BrowserRouter>
   );
 }
